bbb1066978440ac382e47da1bc6631132c6e6157
maksimleononv27 <m1a9k9s6@gmail.com>
2018-04-11 10:14:24 +0300

    Скрипты для установки и запуска под ОС Windows

---
+++ bin/git-back.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+
+node %~dp0..\src\app %1 %2
--- bin/setup.bat
+++ bin/setup.bat
@@ -1,3 +1,17 @@
 @ECHO OFF
+chcp 1251
 
-ECHO "РЎРєСЂРёРїС‚ РґР»СЏ СѓСЃС‚Р°РЅРѕРІРєРё РІ РћРЎ Windows (РІ СЂР°Р·СЂР°Р±РѕС‚РєРµ)"
+ECHO Регистрация схемы...
+
+REG ADD "HKCU\SOFTWARE\Classes\git-back" /ve /t REG_SZ /d "URL:git-back" /f
+REG ADD "HKCU\SOFTWARE\Classes\git-back" /v "URL Protocol" /t REG_SZ /d "" /f
+REG ADD "HKCU\SOFTWARE\Classes\git-back\shell\open\command" /ve /t REG_EXPAND_SZ /d "D:\Projects\donmaks\git-back\bin\git-back.bat %%1" /f
+
+ECHO Добавление в PATH...
+
+IF "%GIT_BACK%"=="" (
+    SETX GIT_BACK %~dp0
+    SETX Path "%Path%;%%GIT_BACK%%"
+) else (
+    SETX GIT_BACK %~dp0
+)
