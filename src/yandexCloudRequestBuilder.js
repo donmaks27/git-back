245d05396052af88da58a5427f3912e3b5e75895
m.leonov <m.leonov@anviovr.com>
2020-05-27 21:25:57 +0300

    Добавил отправку PUT запроса к Яндекс.Облаку

--- src/yandexCloudRequestBuilder.js
+++ src/yandexCloudRequestBuilder.js
@@ -19,13 +19,138 @@ const Crypt = require('./crypt');
 function YandexCloudRequestBuilder (Consts) {
 
     /**
+     * Отправить GET запрос
+     * @param {string} path Путь к запрашиваемому ресурсу (без указания бакета, т.е. /<project>)
+     * @param {{[paramName: string]: string}} queryParams Параметры запроса
+     * @param {(error: bool, responce: http.IncomingMessage, data: string) => void} callback Функция обратного вызова
+     */
+    var SendGetRequest = (path, queryParams, callback) => {
+        if (typeof callback !== 'function') {
+            return;
+        }
+        GetYandexCloudAccount((error, yandexCloudAccount) => {
+            if (error) {
+                Write.file.error('Ошибка чтения yandexCloudAccount');
+                callback(true);
+                return;
+            }
+
+            let host = 'storage.yandexcloud.net';
+            path = '/' + yandexCloudAccount.backetName + path;
+            let dateWithTime = GetDateStringWithTime();
+            let payloadHash = Crypt.sha256_changeEncode('', 'utf8', 'hex');;
+            let headers = {
+                'x-amz-date': dateWithTime,
+                'x-amz-content-sha256': payloadHash
+            };
+
+            let signHeaders = headers;
+            signHeaders['host'] = host;
+            let authorizationHeader = BuildAuthHeader(yandexCloudAccount, 'GET', path, queryParams, signHeaders, payloadHash, dateWithTime);
+            headers['Authorization'] = authorizationHeader;
+
+            if (Object.keys(queryParams).length > 0) {
+                path += '?' + queryParams.join('&');
+            }
+            let request = http.request({
+                method: 'GET',
+                host: host,
+                path: path,
+                headers: headers
+            }, (response) => {
+                // Чтение тела ответа
+                let body = '';
+                response.setEncoding('utf-8');
+                response.on('data', function (chunk) {
+                    body += chunk;
+                });
+                response.on('end', function () {
+                    callback(false, response, body);
+                });
+            });
+    
+            request.on('error', error => {
+                Write.file.error(error.message);
+                callback(true);
+            });
+
+            request.end();
+        });
+    }
+
+    /**
+     * Отправить PUT запрос
+     * @param {string} path Путь к запрашиваемому ресурсу (без указания бакета, т.е. /<project>)
+     * @param {{[paramName: string]: string}} queryParams Параметры запроса
+     * @param {string} data Передаваемые данные
+     * @param {(error: bool, responce: http.IncomingMessage) => void} callback Функция обратного вызова
+     */
+    var SendPutRequest = (path, queryParams, data, callback) => {
+        if (typeof callback !== 'function') {
+            return;
+        }
+        GetYandexCloudAccount((error, yandexCloudAccount) => {
+            if (error) {
+                Write.file.error('Ошибка чтения yandexCloudAccount');
+                callback(true);
+                return;
+            }
+
+            let host = 'storage.yandexcloud.net';
+            path = '/' + yandexCloudAccount.backetName + path;
+            let dateWithTime = GetDateStringWithTime();
+            let payloadHash = Crypt.sha256_changeEncode(data, 'binary', 'hex');
+            let headers = {
+                'x-amz-date': dateWithTime,
+                'x-amz-content-sha256': payloadHash,
+                'x-amz-storage-class': 'COLD'
+            };
+
+            let signHeaders = headers;
+            signHeaders['host'] = host;
+            let authorizationHeader = BuildAuthHeader(yandexCloudAccount, 'PUT', path, queryParams, signHeaders, payloadHash, dateWithTime);
+            headers['Authorization'] = authorizationHeader;
+
+            if (Object.keys(queryParams).length > 0) {
+                path += '?' + queryParams.join('&');
+            }
+            let request = http.request({
+                method: 'PUT',
+                host: host,
+                path: path,
+                headers: headers
+            }, (response) => {
+                callback(false, response);
+            });
+
+            request.on('error', function (error) {
+                Write.file.error(error.message);
+                callback(true);
+            });
+
+            request.write(data, (error) => {
+                if (error) {
+                    Write.file.error('Ошибка добавления данных в запрос');
+                    callback(true);
+                    return;
+                }
+
+                request.end();
+            });
+            // Добавление архива в запрос
+            /*fs.createReadStream(Consts.pathLocalRepoArchive).pipe(request).on('finish', () => {
+                request.end();
+            });*/
+        });
+    }
+
+    /**
      * Проверка наличия аккаунта для Яндекс.Облака
      * @returns {boolean} Файл существует
      */
     var CheckYandexCloudAccount = () => {
         return fs.existsSync(Consts.pathYandexCloudAccount);
     }
-
     /**
      * Получить аккаунт для Яндекс.Облака
      * @param {(error: boolean, account: YandexCloudAccount) => void} callback Функция обратного вызова
@@ -72,83 +197,17 @@ function YandexCloudRequestBuilder (Consts) {
     }
 
     /**
-     * Вычислить хэш данных
-     * @param {string} inData Входные данные в utf8
-     * @returns {string} Хэш данных
-     */
-    var CalculatePayload = (inData) => {
-        return Crypt.sha256_changeEncode(inData, 'utf8', 'hex');
-    }
-
-    /**
-     * Построить GET запрос
-     * @param {string} path Путь к запрашиваемому ресурсу (без указания бакета, т.е. /<project>)
-     * @param {{[paramName: string]: string}} queryParams Параметры запроса
-     * @param {(error: bool, responce: http.IncomingMessage, data: string) => void} callback Функция обратного вызова
-     */
-    var SendGetRequest = (path, queryParams, callback) => {
-        GetYandexCloudAccount((error, yandexCloudAccount) => {
-            if (error) {
-                Write.file.error('Ошибка чтения yandexCloudAccount');
-                callback(true);
-                return;
-            }
-
-            let host = 'storage.yandexcloud.net';
-            path = '/' + yandexCloudAccount.backetName + path;
-            let dateWithTime = GetDateStringWithTime();
-            let dataPayload = CalculatePayload('');
-            let headers = {
-                'x-amz-date': dateWithTime,
-                'x-amz-content-sha256': dataPayload
-            };
-
-            let signHeaders = headers;
-            signHeaders['host'] = host;
-            let authorizationHeader = BuildAuthHeader(yandexCloudAccount, 'GET', path, queryParams, signHeaders, dataPayload, dateWithTime);
-            headers['Authorization'] = authorizationHeader;
-
-            if (Object.keys(queryParams).length > 0) {
-                path += '?' + queryParams.join('&');
-            }
-            let request = http.request({
-                method: 'GET',
-                host: host,
-                path: path,
-                headers: headers
-            }, (response) => {
-                // Чтение тела ответа
-                let body = '';
-                response.setEncoding('utf-8');
-                response.on('data', function (chunk) {
-                    body += chunk;
-                });
-                response.on('end', function () {
-                    callback(false, response, body);
-                });
-            });
-    
-            request.on('error', error => {
-                Write.file.error(error.message);
-                callback(true);
-            });
-
-            request.end();
-        });
-    }
-
-    /**
      * Сгенерировать заголовок Authorization
      * @param {YandexCloudAccount} yandexCloudAccount Данные аккаунта для Яндекс.Облака
      * @param {'GET' | 'PUT'} method Методы запроса
      * @param {string} path Путь к запрашиваемому ресурсу
      * @param {{[paramName: string]: string}} queryParams Параметры запроса
      * @param {{[headerName: string]: string}} headers Заголовки
-     * @param {string} dataPayload Хэш данных
+     * @param {string} payloadHash Хэш данных
      * @param {string} time Время отправки запроса
      * @returns {string} Значение заголовка Authorization
      */
-    var BuildAuthHeader = (yandexCloudAccount, method, path, queryParams, headers, dataPayload, time) => {
+    var BuildAuthHeader = (yandexCloudAccount, method, path, queryParams, headers, payloadHash, time) => {
         let sortedQueryParams = SortMap(queryParams);
         for (let paramIndex = 0; paramIndex < sortedQueryParams.length; paramIndex++) {
             sortedQueryParams[paramIndex] = encodeURI(sortedQueryParams[paramIndex].name) + '=' + encodeURI(sortedQueryParams[paramIndex].value);
@@ -169,7 +228,7 @@ function YandexCloudRequestBuilder (Consts) {
             sortedQueryParams + '\n' +
             sortedHeaders + '\n' +
             sortedHeadersStr + '\n' +
-            dataPayload;
+            payloadHash;
         let hashedCanonicalRequest = Crypt.sha256_changeEncode(canonicalRequest, 'utf8', 'hex');
 
         let date = time.substr(0, time.indexOf('T'));
@@ -211,5 +270,6 @@ function YandexCloudRequestBuilder (Consts) {
     }
 
     this.sendGetRequest = SendGetRequest;
+    this.sendPutRequest = SendPutRequest;
 }
 module.exports = YandexCloudRequestBuilder;
